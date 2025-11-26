06220ebc3afbe1b977da858bcb9cbd5d12e9b537
Dezhao Feng <fengdz@student.ubc.ca>
2025-11-25 21:33:35 -0800

    OrderRepo bug fixed

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++ src/main/java/com/example/demo/repository/OrderRepository.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.Map;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
+
+    // 使用 CAST(... as timestamp) 和 CAST(... as string) 解决 Postgres 参数类型推断问题
     @Query("""
         SELECT o.orderId as orderId,
                o.customerId as customerId,
@@ -21,17 +23,18 @@ public interface OrderRepository extends JpaRepository<Order, String> {
                o.productCategoryNameEnglish as productCategoryNameEnglish
         FROM Order o
         JOIN Customer c ON o.customerId = c.customerId
-        WHERE (o.orderPurchaseTimestamp >= COALESCE(:startDate, o.orderPurchaseTimestamp))
-          AND (o.orderPurchaseTimestamp <= COALESCE(:endDate, o.orderPurchaseTimestamp))
-          AND (c.customerCity = COALESCE(:city, c.customerCity))
-          AND (o.productCategoryNameEnglish = COALESCE(:category, o.productCategoryNameEnglish))
+        WHERE (o.orderPurchaseTimestamp >= COALESCE(CAST(:startDate AS timestamp), o.orderPurchaseTimestamp))
+          AND (o.orderPurchaseTimestamp <= COALESCE(CAST(:endDate AS timestamp), o.orderPurchaseTimestamp))
+          AND (c.customerCity = COALESCE(CAST(:city AS string), c.customerCity))
+          AND (o.productCategoryNameEnglish = COALESCE(CAST(:category AS string), o.productCategoryNameEnglish))
     """)
     List<OrderCityView> findOrdersByFilters (
             @Param("startDate") LocalDateTime startDate,
             @Param("endDate") LocalDateTime endDate,
-            @Param("  city") String city,
+            @Param("city") String city,
             @Param("category") String category
     );
+
     @Query("SELECT o.orderStatus FROM Order o WHERE o.orderId = :orderId")
     String findStatusById(@Param("orderId") String orderId);
 
